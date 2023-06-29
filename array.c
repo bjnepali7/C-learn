06b49d28e90812a80a346d16e7b94bb38bb3c3ed
bjnepali7 <109470940+bjnepali7@users.noreply.github.com>
2023-06-29 20:37:20 +0545

    Update array.c

--- array.c
+++ array.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-void arr(int a[],int size){
+void arr(int a[],int size){  //function for array input and display
 
 int i;
 
-for(i=0;i<size;i++){
+for(i=0;i<size;i++){//taking and input
 printf("enter the value:");
 scanf("%d",&a[i]);
 }
 
 printf("\nthe required values are:");
-for(i=0;i<size;i++){
+for(i=0;i<size;i++){//displaying the array
     printf("%d",a[i]);
 }
 printf("\n");
