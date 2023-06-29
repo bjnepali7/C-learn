a4512be128ed72c726b46b7d4a3a21e59d6d89c9
bjnepali7 <109470940+bjnepali7@users.noreply.github.com>
2023-06-29 20:34:50 +0545

    Update array inserstion.c

--- array inserstion.c
+++ array inserstion.c
@@ -9,23 +9,24 @@ int i;
  printf("%d is size\n",size);
 
 
- for(i=0;i<size;i++)//taking input for array postion\}|
+ for(i=0;i<size;i++){//taking input for array
     scanf("%d",&a[i]);
+ }
 
  printf("enter the position :");
  scanf("%d",&pos);
  printf("enter the num:");
  scanf("%d",&num);
 
- for(i=size-1;i>=pos-1;i--)
+ for(i=size-1;i>=pos-1;i--){//shifting the  elments in array
 
     a[i+1]=a[i];
-
+ }
     a[pos-1]=num;
 
 
 
-    printf("the values are:");
+    printf("the values are:");//displaying the inserted elements in array
     for(i=0;i<size;i++){
         printf("%d\n",a[i]);
     }
